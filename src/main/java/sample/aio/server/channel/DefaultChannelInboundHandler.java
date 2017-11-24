5d842953f3c802bb2e4086167d421ee42674aa16
user <user@192.168.100.114>
2017-11-24 15:27:21 +0800

    1. Followed by aio-server api changing

--- src/main/java/sample/aio/server/channel/DefaultChannelInboundHandler.java
+++ src/main/java/sample/aio/server/channel/DefaultChannelInboundHandler.java
@@ -6,27 +6,33 @@ import com.github.ydydwang.aio.channel.ChannelContext;
 import com.github.ydydwang.aio.channel.ChannelInboundHandler;
 import com.github.ydydwang.aio.util.ByteBufferUtils;
 
-public class DefaultChannelInboundHandler implements ChannelInboundHandler {
+public class DefaultChannelInboundHandler implements ChannelInboundHandler<ByteBuffer, ByteBuffer> {
 
+	@Override
 	public void channelActive(ChannelContext channelContext) throws Exception {
 		System.out.println("channelActive" + channelContext.getRemoteAddress());
 	}
 
+	@Override
 	public void channelInactive(ChannelContext channelContext) throws Exception {
 		System.out.println("channelInactive" + channelContext.getRemoteAddress());
 	}
 
-	public void channelRead(ChannelContext channelContext, ByteBuffer buffer) throws Exception {
+	@Override
+	public ByteBuffer channelRead(ChannelContext channelContext, ByteBuffer buffer) throws Exception {
 		System.out.println("channelRead" + ByteBufferUtils.toString(buffer));
 		System.out.println("channelRead" + channelContext.getRemoteAddress());
+		return buffer;
 	}
 
-	public void exceptionCaught(ChannelContext channelContext, Throwable cause) throws Exception {
-		System.out.println("exceptionCaught" + channelContext.getRemoteAddress());
+	@Override
+	public void readFailed(ChannelContext channelContext, Throwable cause) throws Exception {
+		System.out.println("readFailed");
 	}
 
-	public void channelUnregistered(Throwable cause) throws Exception {
-		System.out.println("channelUnregistered");
+	@Override
+	public void acceptFailed(Throwable cause) throws Exception {
+		System.out.println("acceptFailed");
 	}
 
 }
